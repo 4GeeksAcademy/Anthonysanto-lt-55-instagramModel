f730025a974251592e0daff72d267a399589b573
Anthony González <asgtoribio@gmail.com>
2025-08-19 00:18:06 +0000

    correccion author como key foreign

--- src/models.py
+++ src/models.py
@@ -10,10 +10,12 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    first_Name: Mapped[str] = mapped_column(String(120), unique=False, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(120), unique=False, nullable=False)
     last_name: Mapped[str] = mapped_column(String(120), unique=False, nullable=False)
 
     posts: Mapped[List["Post"]] = relationship(back_populates="user")
+    comments: Mapped[List["Comment"]] = relationship(back_populates="author")
+    
     def serialize(self):
         return {
             "id": self.id,
@@ -37,10 +39,12 @@ class Post(db.Model):
 class Comment(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     comment: Mapped[str] = mapped_column(nullable=False)
-    author_id:Mapped[int] = mapped_column(nullable=False) 
+    
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"),nullable=False)
+    author: Mapped["User"] = relationship(back_populates="comments")
     
     post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
-    post: Mapped["Post"] = relationship(back_populates="comments")    
+    post: Mapped["Post"] = relationship(back_populates="comments")        
 
 class Follower(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
